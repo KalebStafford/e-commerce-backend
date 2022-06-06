7d413dfc5f3c7f8a064e0ceadbada1b6107b89b4
Kaleb Stafford <kalebstafford0@gmail.com>
2022-06-06 13:14:26 -0500

    product tag

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -1,19 +1,38 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
 
 class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        key: "id",
+        model: "tag",
+      },
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        key: "id",
+        model: "product",
+      },
+    },
+    id: {
+      auto_Increment: true,
+      primary_Key: true,
+      allow_Null: false,
+      type: DataTypes.INTEGER,
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product_tag',
+    modelName: "product_tag",
   }
 );
 
